b207643e311852a7f4ee89f557494c5b1e0b97b4
Anthony sabine <sabinanthony@outlook.com>
2019-03-10 11:41:05 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,12 +1,10 @@
+def display_board(board)
+separator = "|"
+lines = "-----------"
 
-def display_board
-  cell = "   "
-  separator = "|"
-  lines = "-----------"
-
-  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
-  puts "#{lines}"
-  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
-  puts "#{lines}"
-  puts "#{cell}#{separator}#{cell}#{separator}#{cell}"
+puts " #{board[0]} #{separator} #{board[1]} #{separator} #{board[2]} "
+puts "#{lines}"
+puts " #{board[3]} #{separator} #{board[4]} #{separator} #{board[5]} "
+puts "#{lines}"
+puts " #{board[6]} #{separator} #{board[7]} #{separator} #{board[8]} "
 end
